2da5d14dd8a932fe07e4a20a13ca0b5e1bae5a48
Ryan Mcpherson <mmcpher2@gmail.com>
2017-12-13 09:57:01 -0600

    Chat board working and added CDNS for GH Pages

--- app/chatboard/controllers/ChatboardCtrl.js
+++ app/chatboard/controllers/ChatboardCtrl.js
@@ -1,7 +1,7 @@
 angular
 .module("RyanSays")
 .controller("ChatboardCtrl",
-function ($scope, $location, AuthFactory, UserFactory, ChatFactory) {
+function ($scope, $location, AuthFactory, UserFactory, ChatFactory, $timeout) {
 
     $scope.userChats = []
 
@@ -14,16 +14,35 @@ function ($scope, $location, AuthFactory, UserFactory, ChatFactory) {
         }
     })
 
+    // Setting date structure up for addChat()
+    let today = new Date()
+    let dd = today.getDate()
+    let mm = today.getMonth()+1
+    let yyyy = today.getFullYear()
+   if(dd<10) {
+        dd = "0"+dd
+    }
+   if(mm<10) {
+        mm = "0"+mm
+    }
+   today = mm + "/" + dd + "/" + yyyy
+
+
     $scope.addChat = function(newChat) {
         chatObj =   {
             "userId": $scope.userObj.uid,
             "firstName": $scope.userObj.firstName,
             "lastName": $scope.userObj.lastName,
-            "datePosted": Date.now(),
+            "datePosted": today,
             "comment": $scope.newChat
         }
         // Adding new user object to FB through UserFactory add "POST" function
-        ChatFactory.addChat(chatObj)
+        ChatFactory.addChat(chatObj).then(function () {
+            // $scope.userChats = ""
+            ChatFactory.listChats().then(function (response) {
+                    $scope.userChats = response
+            })
+        })
         $scope.newChat = ""
     }
     ChatFactory.listChats().then(function (response) {
