3c03cf460b9812b0809b9b460b570c4cdc62a669
Ryan Mcpherson <mmcpher2@gmail.com>
2017-12-12 19:10:14 -0600

    Comments are being added to fb

--- app/chatboard/controllers/ChatboardCtrl.js
+++ app/chatboard/controllers/ChatboardCtrl.js
@@ -1,25 +1,24 @@
 angular.module("RyanSays").controller("ChatboardCtrl",
 function ($scope, $location, AuthFactory, UserFactory, ChatFactory) {
 
+    // Get current user
+    $scope.user = AuthFactory.getUser()
+    // Get the single current user that has the same uid and userID and get that user FB KEY
+    UserFactory.single($scope.user.uid).then((user)=>{
+        for (var key in user) {
+            $scope.userObj = user[key]
+        }
+    })
 
-    // $scope.addChat = function() {
-    //       chatObj =   {
-    //           "userId": null,
-    //           "firstName": null,
-    //           "lastName": null,
-    //           "datePosted": null,
-    //           "comment": null
-    //       }
-    //       // Adding new user object to FB through UserFactory add "POST" function
-    //       ChatFactory.addChat(chatObj)
-    //     }
-
-    // // Get current user
-    // $scope.user = AuthFactory.getUser()
-    // // Get the single current user that has the same uid and userID and get that user FB KEY
-    // UserFactory.single($scope.user.uid).then((user)=>{
-    //     for (var key in user) {
-    //         $scope.userObj = user[key]
-    //     }
-    // })
+    $scope.addChat = function(newChat) {
+          chatObj =   {
+              "userId": $scope.userObj.uid,
+              "firstName": $scope.userObj.firstName,
+              "lastName": $scope.userObj.lastName,
+              "datePosted": Date.now(),
+              "comment": $scope.newChat
+          }
+          // Adding new user object to FB through UserFactory add "POST" function
+          ChatFactory.addChat(chatObj)
+        }
 })
--- app/chatboard/factories/ChatFactory.js
+++ app/chatboard/factories/ChatFactory.js
@@ -30,7 +30,6 @@ angular
                     data: chatObj
                 })
             }
-        },
-
+        }
     })
 })
